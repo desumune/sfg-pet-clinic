c5f7554d73b3201a68a318e064154ac8df9adfbc
desumune <czarek7711@gmail.com>
2020-03-02 21:08:42 +0100

    Adding Owners Lsit and updated controller. closes #16

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -29,9 +29,9 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Fiona");
-        owner1.setLastName("Kowalski");
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Kowalski");
 
         ownerService.save(owner2);
 
